22ad278f9968d1e2616e6cca878083a4e9bb3502
blockparty <hello@blockparty.sh>
2018-12-22 23:27:29 -0600

    import txs from mempool, using new getrawmempooltxs rpc call

---
+++ import-txs-from-mempool.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os 
+import json
+import base64
+import argparse
+import binascii
+import pymongo
+from dotenv import load_dotenv
+from blockchain_parser.transaction import Transaction
+import bitcoin.rpc
+import tna
+
+load_dotenv()
+
+btc_conf_file = os.path.expanduser(os.getenv("BTC_CONF_FILE"))
+
+mongo = pymongo.MongoClient(os.getenv('MONGO_URL'))
+db = mongo[os.getenv('MONGO_NAME')]
+#db.confirmed.delete_many({}) # clear old data (done in parallel)
+
+parser = argparse.ArgumentParser(description="import transactions to bitdb")
+parser.add_argument("--dry", action="store_true", help="dry run (no inserts)")
+parser.add_argument("--verbose", action="store_true", help="show json from tna")
+args = parser.parse_args()
+
+rpc = bitcoin.rpc.RawProxy(btc_conf_file=btc_conf_file)
+
+documents = []
+transactions = rpc.getrawmempooltxs()
+total_transactions = len(transactions)
+for txdata_index, txdata in enumerate(transactions):
+    tx = Transaction.from_hex(binascii.unhexlify(txdata))
+    res = tna.extract(None, tx)
+    if args.verbose:
+        print(json.dumps(res, indent=4))
+    documents.append(res)
+
+    print("{}%\t{}/{}".format(
+        round((txdata_index / total_transactions) * 100, 2),
+        txdata_index,
+        total_transactions
+    ))
+
+if not args.dry:
+    inserted = len(db.confirmed.insert_many(documents).inserted_ids)
+    print("inserted={}".format(inserted))
