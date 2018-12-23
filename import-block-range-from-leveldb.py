05c3c1c5132a0dfec0948e6c867e5efd1915141f
blockparty <hello@blockparty.sh>
2018-12-22 23:18:45 -0600

    use tna module + add dry, verbose flags

--- import-block-range-from-leveldb.py
+++ import-block-range-from-leveldb.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
 import os 
-import json
-import base64
 import argparse
 import pymongo
+import json
 from dotenv import load_dotenv
 from blockchain_parser.blockchain import Blockchain
-from blockchain_parser.address import Address
-from bitcoin.core.script import CScriptOp
-from cashaddress import convert
+import tna
+
 
 load_dotenv()
 
@@ -33,6 +31,8 @@ except FileNotFoundError:
 parser = argparse.ArgumentParser(description="import transactions to bitdb")
 parser.add_argument("--start-block", type=int, required=True, help="block to start")
 parser.add_argument("--end-block", type=int, required=True, help="block to start")
+parser.add_argument("--dry", action="store_true", help="dry run (no inserts)")
+parser.add_argument("--verbose", action="store_true", help="show json from tna")
 args = parser.parse_args()
 
 for block in blockchain.get_ordered_blocks(
@@ -43,91 +43,19 @@ for block in blockchain.get_ordered_blocks(
 ):
     #print("height=%d block=%s" % (block.height, block.hash))
     documents = []
-    for tx_index, tx in enumerate(block.transactions):
-        #print("tx: ", tx)
-        #print(tx_index)
-        inputs = []
-        outputs = []
+    for tx in block.transactions:
+        res = tna.extract(block, tx)
+        if args.verbose:
+            print(json.dumps(res, indent=4))
+        documents.append(res)
 
-        for input_index, item in enumerate(tx.inputs):
-            xput = { "i": input_index }
+    if args.dry:
+        inserted = 0
+    else:
+        inserted = len(db.confirmed.insert_many(documents).inserted_ids)
 
-            if not tx.is_coinbase(): # coinbase input doesnt have to be valid script
-                for chunk_index, chunk in enumerate(item.script.operations):
-                    if isinstance(chunk, bytes):
-                        xput["b" + str(chunk_index)] = base64.b64encode(chunk).decode("utf-8")
-                        xput["h" + str(chunk_index)] = chunk.hex()
-                    elif isinstance(chunk, CScriptOp):
-                        xput["b" + str(chunk_index)] = { "op": int(chunk) }
-                    else:
-                        xput["b" + str(chunk_index)] = chunk
-
-            xput["str"] = item.script.value
-            sender = {
-                "h": item.transaction_hash,
-                "i": item.transaction_index,
-            }
-
-            addr = None
-
-            # TODO add additional address types
-            if not tx.is_coinbase():
-                if len(item.script.operations) == 2: # p2pk
-                    if isinstance(item.script.operations[1], str): # could be CScriptOp in rare case
-                        if len(item.script.operations[1]) == 33:
-                            addr = Address.from_public_key(item.script.operations[1]).address
-
-            if addr is not None:
-                sender['a'] = convert.to_cash_address(addr)[12:] # remove "bitcoincash:" prefix
-
-            xput["e"] = sender
-            inputs.append(xput)
-
-        for output_index, item in enumerate(tx.outputs):
-            xput = { "i": output_index }
-
-            for chunk_index, chunk in enumerate(item.script.operations):
-                if isinstance(chunk, bytes):
-                    xput["b" + str(chunk_index)] = base64.b64encode(chunk).decode("utf-8")
-                    try:
-                        xput["s" + str(chunk_index)] = chunk.decode("utf-8")
-                    except UnicodeDecodeError:
-                        pass
-                    xput["h" + str(chunk_index)] = chunk.hex()
-                elif isinstance(chunk, CScriptOp):
-                    xput["b" + str(chunk_index)] = { "op": int(chunk) }
-                else:
-                    xput["b" + str(chunk_index)] = chunk
-
-            xput["str"] = item.script.value
-
-            receiver = {
-                "v": item.value,
-                "i": output_index
-            }
-            # bitdb only supports single address, so we will too
-            addresses = [str(m.address) for m in item.addresses]
-            if len(addresses) == 1:
-                receiver["a"] = convert.to_cash_address(addresses[0])[12:] # remove "bitcoincash:" prefix
-            xput["e"] = receiver
-
-            outputs.append(xput)
-
-
-        d = {
-            "tx": { "h": tx.hash },
-            "in": inputs,
-            "out": outputs,
-            "blk": {
-                "i": block.height,
-                "h": block.hash,
-                "t": block.header.timestamp.strftime("%s")
-            }
-        }
-        documents.append(d)
-        #print(json.dumps(d, indent=4))
     print("{}%\theight={} inserted={}".format(
         round((block.height-args.start_block) / (args.end_block-args.start_block) * 100, 2),
         block.height,
-        len(db.confirmed.insert_many(documents).inserted_ids))
-    )
+        inserted
+    ))
