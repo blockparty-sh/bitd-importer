585882658a8d94a4e1eb7e426b8dd4060352f6c9
blockparty <hello@blockparty.sh>
2018-12-28 22:15:17 -0600

    add index creator

---
+++ create-indexes.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import os
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mongo = pymongo.MongoClient(os.getenv('MONGO_URL'))
+db = mongo[os.getenv('MONGO_NAME')]
+
+def add_indexes_to_collection(collection):
+    print('adding blk.i')
+    collection.create_index("blk.i")
+    print('adding blk.h')
+    collection.create_index("blk.h")
+    print('adding blk.t')
+    collection.create_index("blk.t")
+    print('adding tx.h')
+    collection.create_index("tx.h")
+
+    print('adding in.e.i')
+    collection.create_index("in.e.i")
+    print('adding in.e.a')
+    collection.create_index("in.e.a")
+    print('adding in.e.h')
+    collection.create_index("in.e.h")
+    print('adding in.i')
+    collection.create_index("in.i")
+
+    print('adding out.e.a')
+    collection.create_index("out.e.a")
+    print('adding out.e.v')
+    collection.create_index("out.e.v")
+    print('adding out.e.i')
+    collection.create_index("out.e.i")
+    print('adding out.i')
+    collection.create_index("out.i")
+
+    for i in range(0, 16):
+        print('adding in.h1', i)
+        collection.create_index("in.h1")
+        print('adding in.b1', i)
+        collection.create_index("in.b1")
+
+        print('adding out.s1', i)
+        collection.create_index("out.s1")
+        print('adding out.s1 (FULLTEXT)', i)
+        collection.create_index([("out.s1", pymongo.TEXT)])
+        print('adding out.h1', i)
+        collection.create_index("out.h1")
+        print('adding out.b1', i)
+        collection.create_index("out.b1")
+
+print('CONFIRMED')
+add_indexes_to_collection(db.confirmed)
+print('UNCONFIRMED')
+add_indexes_to_collection(db.unconfirmed)
