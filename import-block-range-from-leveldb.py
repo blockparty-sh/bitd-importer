73f667bed69f7736a4b9c9253f27750cb6b5e2aa
blockparty <hello@blockparty.sh>
2018-12-29 05:18:07 -0600

    bulk queue by transaction count instead of by block

--- import-block-range-from-leveldb.py
+++ import-block-range-from-leveldb.py
@@ -23,6 +23,7 @@ db = mongo[os.getenv('MONGO_NAME')]
 parser = argparse.ArgumentParser(description="import transactions to bitdb")
 parser.add_argument("--start-block", type=int, required=True, help="block to start")
 parser.add_argument("--end-block", type=int, help="block to finish on, if not given will get last one in index cache")
+parser.add_argument("--bulk-amount", type=int, default=100000, help="how many documents to process before doing insert")
 parser.add_argument("--dry", action="store_true", help="dry run (no inserts)")
 parser.add_argument("--verbose", action="store_true", help="show json from tna")
 args = parser.parse_args()
@@ -33,6 +34,7 @@ util.build_index_cache(index_path, cache_path, blockchain)
 if not args.end_block:
     args.end_block = util.count_leveldb_last_block(index_path, cache_path, start_block, blockchain)
 
+documents = []
 for block in blockchain.get_ordered_blocks(
     index=index_path,
     cache=cache_path,
@@ -40,20 +42,24 @@ for block in blockchain.get_ordered_blocks(
     end=args.end_block
 ):
     #print("height=%d block=%s" % (block.height, block.hash))
-    documents = []
     for tx in block.transactions:
         res = tna.extract(block, tx)
         if args.verbose:
             print(json.dumps(res, indent=4))
         documents.append(res)
 
-    if args.dry:
-        inserted = 0
-    else:
-        inserted = len(db.confirmed.insert_many(documents).inserted_ids)
+        if not args.dry and len(documents) >= args.bulk_amount:
+            inserted = len(db.confirmed.insert_many(documents).inserted_ids)
+            documents = []
+            print("inserted {} documents".format(inserted))
 
-    print("{}%\theight={} inserted={}".format(
+    print("{}%\theight={}\ttxs={}".format(
         round((block.height-args.start_block) / (args.end_block-args.start_block) * 100, 2),
         block.height,
-        inserted
+        len(block.transactions)
     ))
+
+# insert remaining documents
+if not args.dry:
+    inserted = len(db.confirmed.insert_many(documents).inserted_ids)
+    print("inserted {} documents".format(inserted))
