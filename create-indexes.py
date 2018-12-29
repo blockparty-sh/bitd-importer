8d92177728396242ba7291da50fffe747ce42118
blockparty <hello@blockparty.sh>
2018-12-28 23:31:37 -0600

    change depth of indexed fields, make tx.h uniq

--- create-indexes.py
+++ create-indexes.py
@@ -10,14 +10,18 @@ mongo = pymongo.MongoClient(os.getenv('MONGO_URL'))
 db = mongo[os.getenv('MONGO_NAME')]
 
 def add_indexes_to_collection(collection):
+    print('dropping old indexes')
+    collection.drop_indexes();
+
+    print('adding tx.h')
+    collection.create_index("tx.h", unique=True)
+
     print('adding blk.i')
     collection.create_index("blk.i")
     print('adding blk.h')
     collection.create_index("blk.h")
     print('adding blk.t')
     collection.create_index("blk.t")
-    print('adding tx.h')
-    collection.create_index("tx.h")
 
     print('adding in.e.i')
     collection.create_index("in.e.i")
@@ -37,19 +41,21 @@ def add_indexes_to_collection(collection):
     print('adding out.i')
     collection.create_index("out.i")
 
-    for i in range(0, 16):
-        print('adding in.h1', i)
+    for i in range(0, 8):
+        print('adding in.h{}'.format(i))
         collection.create_index("in.h"+str(i))
-        print('adding in.b1', i)
+
+        print('adding in.b{}'.format(i))
         collection.create_index("in.b"+str(i))
 
-        print('adding out.s1', i)
+        print('adding out.s'.format(i))
         collection.create_index("out.s"+str(i))
-        print('adding out.s1 (FULLTEXT)', i)
-        collection.create_index([("out.s"+str(i), pymongo.TEXT)])
-        print('adding out.h1', i)
+        print('adding out.s{} (FULLTEXT)'.format(i))
+        #collection.drop_index("out.s"+str(i)+"_text")
+        #collection.create_index([("out.s"+str(i), pymongo.TEXT)])
+        print('adding out.h{}'.format(i))
         collection.create_index("out.h"+str(i))
-        print('adding out.b1', i)
+        print('adding out.b{}'.format(i))
         collection.create_index("out.b"+str(i))
 
 print('CONFIRMED')
